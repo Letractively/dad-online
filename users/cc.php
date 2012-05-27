8cb431413288504fffc5b0914b53e013c1b0bc9e
davidlyon1988 <davidlyon1988@localhost>
2012-05-26 22:05:44 -0530

    v0.1.15.2 - Minor Bugfix

--- users/cc.php
+++ users/cc.php
@@ -23,12 +23,21 @@ This program is free software: you can redistribute it and/or modify it under th
 
 	// Get classes list
 
-	$query = "SELECT name FROM classes";
+	$query = "SELECT id,name FROM classes";
 	$result = mysql_query($query) or die(mysql_error());
 
 	// Load HTML5 Template
 
     $depth = "../aat/"; include_once '../aat/header.php';
+
+	// Class/Race Selector
+
+	echo '<script language=javascript>
+			function reload(form) {
+			var val=form.class.options[form.class.options.selectedIndex].value;
+			self.location="cc.php?class=" + val;
+		}
+		</script>';
 
 	// Characer Creation Form
 
@@ -39,13 +48,13 @@ This program is free software: you can redistribute it and/or modify it under th
 			<label for="name">Name:</label>
 			<input type="text" id="name" name="name" maxlength="16" required/><br/>
 			<label for="race">Class:</label>
-			<select name="class" onChange="changePicture()" required>
+			<select name="class" onChange="reload(this.form)" required>
 			<option value="">Please select a class</option>';
 
 	// Fill selection with available classes
 
 	while($row = mysql_fetch_array($result)) {
-		echo '<option value="'.$row[0].'">'.$row[0].'</option>';
+		echo '<option value="'.$row[0].'">'.$row[1].'</option>';
 	}
 
 	// Character Creation Form end + Race picture preview
